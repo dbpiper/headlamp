f07217d2db4cad4a2ed344b99f5936cd23ecc5da
David Piper <dbpiper@gmail.com>
2025-12-31 18:03:30 -0600

    Don't try to build dioxus on ci

--- treemap_viewer/src/main.rs
+++ treemap_viewer/src/main.rs
@@ -1,8 +1,21 @@
+#[cfg(feature = "desktop")]
 mod app;
+#[cfg(feature = "desktop")]
 mod ui;
+#[cfg(feature = "desktop")]
 mod util;
+#[cfg(feature = "desktop")]
 mod view;
 
+#[cfg(feature = "desktop")]
 fn main() {
     dioxus::launch(app::App);
+}
+
+#[cfg(not(feature = "desktop"))]
+fn main() {
+    eprintln!(
+        "treemap_viewer desktop UI is disabled. Rebuild with `-p treemap_viewer --features desktop`."
+    );
+    std::process::exit(1);
 }
